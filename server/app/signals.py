eb828262d67f3e60ff23afec3525425ab56bb772
NoPasaRun <bogdanbelenesku@gmail.com>
2023-07-14 21:54:01 +0300

    поменял логику удаления файлов при обновлении данных

--- server/app/signals.py
+++ server/app/signals.py
@@ -15,7 +15,9 @@ def remove_file(sender, instance, **kwargs) -> None:  # pylint: disable=unused-a
     if not instance._state.adding:  # pylint: disable=protected-access
         obj = sender.objects.get(id=instance.id)
         files = filter(lambda field: hasattr(obj, field), ["image", "video"])
-        for file in files:
+        for file in filter(
+            lambda field: getattr(instance, field) != getattr(obj, field), files
+        ):
             rel_file_path = str(getattr(obj, file))
             abs_file_path = os.path.join(settings.MEDIA_ROOT, rel_file_path)
             if os.path.exists(abs_file_path):
